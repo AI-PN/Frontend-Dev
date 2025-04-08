47ee8f48f34ced4c656a54d5b1bec92f7588463c
Kajol Dubey <kajoldubey15@gmail.com>
2025-04-08 15:31:15 -0400

    setup

--- .next/types/app/register/page.ts
+++ .next/types/app/register/page.ts
@@ -1,4 +1,4 @@
-// File: /Users/jhansipatnala/Frontend-Dev/src/app/register/page.js
+// File: /Users/kajoldubey/Documents/Patient-navigator/Frontend-Dev/src/app/register/page.js
 import * as entry from '../../../../src/app/register/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
